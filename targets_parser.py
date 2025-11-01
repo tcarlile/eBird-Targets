999efa34cdfe52a01433fe78a13011ccd7dc6e9a
tcarlile <tcarlile@gmail.com>
2025-11-01 15:53:02 -0400

    Update targets_parser.py

--- targets_parser.py
+++ targets_parser.py
@@ -52,11 +52,11 @@ def parseTargets(session, hs, targets):
 	targLenB = len(targets) # Length of targets list before parsing URL
 	for label in labels: # Iterate through categories
 		for section in soup.find_all('section', {'aria-labelledby' : label } ):
-			for target in section.find_all('li'): # Find all species elements, <li>, iterate, and parse
-				indx = target.find('div', {'class' : 'ResultsStats-index'} ).getText().strip().strip('.')
-				elem = target.find('div', {'class' : 'SpecimenHeader'} ) # Gets <div> with species info
-				urls = 'https://ebird.org/species/' + elem.find('a').get('data-species-code') # Spuh URL
-				spuh = elem.getText().strip() # Get species name
+			for target in section.find_all('li'): # Find all species, <li> element per spuh, iterate, parse
+				indx = target.find('div', {'class' : 'ResultsStats-index'}).getText().strip().strip('.')
+				elem = target.find('div', {'class' : 'SpecimenHeader'}) 
+				urls = 'https://ebird.org/species/'+elem.find('a').get('data-species-code')
+				spuh = elem.getText().strip()
 				freq = target.find('div', {'class' : 'ResultsStats-stats'}).get('title').strip('.% frequency')
 				targets.append([indx,spuh,freq,urls,name])
 	if targLenB == len(targets): # Occurs when target species data is empty
